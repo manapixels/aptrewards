0503a8327c59f7f83c09a9e55f761bbe1cb1da46
manapixels <chrenelias@outlook.com>
2024-09-22 10:36:12 +0800

    apply new style to program name header

--- src/components/admin/edit/EditProgramForm.tsx
+++ src/components/admin/edit/EditProgramForm.tsx
@@ -49,7 +49,14 @@ export default function EditProgramForm({ programId }: { programId: string }) {
         <div className="flex flex-col gap-4">
             <div>
                 <div className="text-gray-500 text-sm">Rewards Program</div>
-                <h1 className="font-bold text-2xl ml-2 my-4"><div className="inline bg-gray-100 px-2 py-1 h-100 mr-2"></div>{currProgram?.name}</h1>
+                <div className="flex items-center my-2">
+                    <div className="h-auto w-8 patterned-placeholder self-stretch"></div>
+                    <h1 className="font-bold text-2xl px-3 py-3">
+                        {/* <div className="inline bg-gray-100 px-2 py-1 h-100 mr-2"></div> */}
+                        {currProgram?.name}
+                    </h1>
+                    <div className="flex-1 h-auto w-auto patterned-placeholder self-stretch"></div>
+                </div>
             </div>
             {currProgram && (
                 <>
